b9bc42a9783d04bdea5b44b2f24eca0163d1d1ac
Jero <jeraviah@gmail.com>
2020-05-27 19:02:42 +1200

    Update View

--- Bug_Tracking_System/Startup.cs
+++ Bug_Tracking_System/Startup.cs
@@ -15,12 +15,14 @@ namespace Bug_Tracking_System
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -28,19 +30,74 @@ namespace Bug_Tracking_System
             services.AddControllersWithViews();
             
             services.AddDbContext<BugContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("BugContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("BugContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
 
             services.AddDbContext<UserContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("UserContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("UserContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
 
             services.AddDbContext<EnrollmentContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("EnrollmentContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("EnrollmentContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
 
             services.AddDbContext<ProjectContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("ProjectContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("ProjectContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
 
             services.AddDbContext<TestCaseContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("TestCaseContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("TestCaseContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
